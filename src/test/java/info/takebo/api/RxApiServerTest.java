68897e27c7f272fb14916d44d62b5c1139be6f31
yamashita takeshi <yamashita_takeshi@cyberagent.co.jp>
2015-03-20 23:35:43 +0900

    test

--- src/test/java/info/takebo/api/RxApiServerTest.java
+++ src/test/java/info/takebo/api/RxApiServerTest.java
@@ -1,5 +1,6 @@
 package info.takebo.api;
 
+import static org.hamcrest.CoreMatchers.*;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -13,6 +14,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.net.HttpHeaders;
 
 @RunWith(VertxUnitRunner.class)
 public class RxApiServerTest {
@@ -32,7 +35,8 @@ public class RxApiServerTest {
 	}
 
 	@After
-	public void after(TestContext test) {}
+	public void after(TestContext test) {
+	}
 
 	@Test
 	public void testHello(TestContext test) {
@@ -44,6 +48,25 @@ public class RxApiServerTest {
 				if (res.statusCode() != 200) {
 					test.fail("status.code -> " + res.statusCode());
 				}
+				return res.toObservable();
+			})
+			.subscribe(data -> {
+				logger.info("Server content " + data.toString("UTF-8"));
+			});
+
+		req.end();
+	}
+
+	@Test
+	public void testIndex(TestContext test) {
+		HttpClient client = Vertx.vertx().createHttpClient();
+		HttpClientRequest req = client.request(HttpMethod.GET, 8080, "localhost", "/");
+
+		req.toObservable()
+			.flatMap(res -> {
+				test.assertEquals(res.statusCode(), 303);
+				test.assertEquals(res.headers().get(HttpHeaders.LOCATION), is("hello"));
+
 				return res.toObservable();
 			})
 			.subscribe(data -> {
