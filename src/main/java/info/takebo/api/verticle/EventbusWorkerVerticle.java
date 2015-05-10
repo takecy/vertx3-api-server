80fd592990d2834d0441fd7230e04d8c6d8a80bc
takeshi yamashita <yamashita_takeshi@cyberagent.co.jp>
2015-05-10 23:17:22 +0900

    eventbus handler

--- src/main/java/info/takebo/api/verticle/EventbusWorkerVerticle.java
+++ src/main/java/info/takebo/api/verticle/EventbusWorkerVerticle.java
@@ -3,7 +3,13 @@
  */
 package info.takebo.api.verticle;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.eventbus.EventBus;
+import io.vertx.rxjava.core.eventbus.MessageConsumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +19,34 @@ import org.slf4j.LoggerFactory;
  */
 public class EventbusWorkerVerticle extends AbstractVerticle {
 
-	private Logger logger = LoggerFactory.getLogger("init.server" + EventbusWorkerVerticle.class.getName());
+	private Logger logger = LoggerFactory.getLogger("init.server"
+			+ EventbusWorkerVerticle.class.getName());
 
 	@Override
 	public void start() throws Exception {
 
-		// TODO
+		EventBus eb = vertx.eventBus();
+
+		MessageConsumer<String> consumer = eb.consumer("news.uk.sport");
+
+		// eb receive event handler
+		consumer.handler(message -> {
+			logger.info("I have received a message: " + message.body());
+			logger.info("message headers: " + message.headers());
+		});
+
+		// eb send event handler
+		consumer.completionHandler(res -> {
+			if (res.succeeded()) {
+				logger.info("The handler registration has reached all nodes");
+			} else {
+				logger.info("Registration failed!");
+			}
+		});
+
+
+		DeliveryOptions options = new DeliveryOptions();
+		options.addHeader("x-token-header", "token-" + Calendar.getInstance().getTimeInMillis());
+		eb.send("news.uk.sport", "Yay! Someone kicked a ball", options);
 	}
 }
